c6e1a92c11868726c871dd00ba672118bdc9322e
Ezra <dulaneyezra@gmail.com>
2024-08-08 13:13:40 -0400

    fixes

--- client/src/components/Item/Item.jsx
+++ client/src/components/Item/Item.jsx
@@ -1,45 +1,47 @@
-import { Box, Heading, Skeleton, Text } from "@chakra-ui/react";
+import { Box, Heading, Skeleton, Stack, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom"
 
 export default function Item() {
-const param = useParams();
-const [item, setItem] = useState({});
-const [user, setUser] = useState({});
+    const param = useParams();
+    const [item, setItem] = useState({});
+    const [user, setUser] = useState({});
 
-useEffect(() => {
-    fetch(`http://localhost:3000/item/${param.id}`)
-    .then(res => res.json())
-    .then(json => setItem(json))
-// eslint-disable-next-line react-hooks/exhaustive-deps
-}, [])
+    useEffect(() => {
+        fetch(`http://localhost:3000/item/${param.id}`)
+            .then(res => res.json())
+            .then(json => setItem(json))
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-useEffect(() => {
-    if(Object.keys(item).length > 1) {
-        fetch(`http://localhost:3000/user/${item.user_id}`)
-        .then(res => res.json())
-        .then(json => setUser(json))
+    useEffect(() => {
+        if (Object.keys(item).length > 1) {
+            fetch(`http://localhost:3000/user/${item.user_id}`)
+                .then(res => res.json())
+                .then(json => setUser(json))
+        }
+    }, [item])
+
+    if (Object.keys(item).length < 1) {
+        return (
+            <Skeleton />
+        )
     }
-}, [item])
 
-if(Object.keys(item).length < 1) {
-    return(
-        <Skeleton/>
-    )
-}
-
-    return(
+    return (
         <>
-        <Helmet>
-            <title>{item.item_name} Details</title>
-        </Helmet>
-        <Box boxShadow='md' borderWidth='1px' borderRadius='lg' marginY="1em" paddingY="5em" maxW="50%" margin="0 auto" textAlign="center">
-            <Heading>{item.item_name}</Heading>
-            <Text>Description: {item.description}</Text>
-            <Text>Quantity: {item.quantity}</Text>
-            <Text>Posted by: {user.first_name} ({user.username})</Text>
-        </Box>
+            <Helmet>
+                <title>{item.item_name} Details</title>
+            </Helmet>
+            <Box boxShadow='md' borderWidth='1px' borderRadius='lg' marginY="1em" paddingY="5em" maxW="50%" margin="0 auto" textAlign="center">
+                <Stack spacing="1em">
+                    <Heading>{item.item_name}</Heading>
+                    <Text>Description: {item.description}</Text>
+                    <Text>Quantity: {item.quantity}</Text>
+                    <Text>Posted by: {user.first_name} ({user.username})</Text>
+                </Stack>
+            </Box>
         </>
     )
 }
